6a230747bf8eb7da270e4d21d107f13f3a25689e
Doolally <anshul@brewcraftsindia.com>
2018-01-31 16:45:14 +0530

    menu redesigned

--- application/views/HeaderView.php
+++ application/views/HeaderView.php
@@ -20,111 +20,118 @@
             ?>
             <ul class="nav navbar-nav">
                 <li><a href="<?php echo base_url();?>"><i class="fa fa-home"></i> Home</a></li>
-                <?php
-                if(isSessionVariableSet($this->isUserSession) === true)
-                {
-                    if(myInArray('maintenance',$userModules))
-                    {
-                        ?>
-                        <li><a href="<?php echo base_url();?>maintenance"><i class="fa fa-cogs"></i> Maintenance</a></li>
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#"><i class="fa fa-bars"></i> Menu
+                        <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
                         <?php
-                    }
-                    if(myInArray('quiz',$userModules))
-                    {
-                        if($this->userType == SERVER_USER)
+                        if(isSessionVariableSet($this->isUserSession) === true)
                         {
-                            if(myIsArray($quizNames))
+                            if(myInArray('maintenance',$userModules))
                             {
                                 ?>
-                                <li><a href="<?php echo base_url();?>quiz"><i class="fa fa-question-circle-o"></i> Quiz</a></li>
+                                <li><a href="<?php echo base_url();?>maintenance"><i class="fa fa-cogs"></i> Maintenance</a></li>
+                                <?php
+                            }
+                            if(myInArray('quiz',$userModules))
+                            {
+                                if($this->userType == SERVER_USER)
+                                {
+                                    if(myIsArray($quizNames))
+                                    {
+                                        ?>
+                                        <li><a href="<?php echo base_url();?>quiz"><i class="fa fa-question-circle-o"></i> Quiz</a></li>
+                                        <?php
+                                    }
+                                }
+                                else
+                                {
+                                    ?>
+                                    <li><a href="<?php echo base_url();?>quiz"><i class="fa fa-question-circle-o"></i> Quiz</a></li>
+                                    <?php
+                                }
+                            }
+                            if(myInArray('quiz_questions',$userModules))
+                            {
+                                ?>
+                                <li><a href="<?php echo base_url();?>quiz/manageQuestions"><i class="fa fa-list-ol"></i> Quiz Questions</a></li>
+                                <?php
+                            }
+                            if(myInArray('career',$userModules))
+                            {
+                                ?>
+                                <li><a href="<?php echo base_url();?>career"><i class="fa fa-suitcase"></i> Careers</a></li>
+                                <?php
+                            }
+                            if(myInArray('mug_club',$userModules))
+                            {
+                                ?>
+                                <li><a href="<?php echo base_url();?>mugclub"><i class="fa fa-beer"></i> Mug Club</a></li>
+                                <?php
+                            }
+
+                            if(myInArray('wallet_check',$userModules))
+                            {
+                                ?>
+                                <li><a href="<?php echo base_url().'wallet';?>"><i class="fa fa-money"></i> Wallet Check</a></li>
+                                <?php
+                            }
+
+                            if(myInArray('wallet_users',$userModules))
+                            {
+                                ?>
+                                <li><a href="<?php echo base_url().'empDetails';?>"><i class="fa fa-users"></i> Wallet Users</a></li>
+                                <?php
+                            }
+
+                            if(myInArray('user_mgt',$userModules))
+                            {
+                                ?>
+                                <li><a href="<?php echo base_url();?>users"><i class="fa fa-user"></i> Users List</a></li>
+                                <?php
+                            }
+
+                            if(myInArray('location_mgt',$userModules))
+                            {
+                                ?>
+                                <li><a href="<?php echo base_url();?>locations"><i class="fa fa-globe"></i> Locations</a></li>
+                                <?php
+                            }
+
+                            if(myInArray('mailers',$userModules))
+                            {
+                                ?>
+                                <li><a href="<?php echo base_url();?>mailers"><i class="fa fa-envelope"></i> Mailers</a>
+                                    <div class="notification-indicator"></div>
+                                </li>
+                                <?php
+                            }
+
+                            if(myInArray('offers',$userModules))
+                            {
+                                ?>
+                                <li><a href="<?php echo base_url();?>offers"><i class="fa fa-trophy"></i> Offers</a></li>
+                                <?php
+                            }
+
+                            if(myInArray('offers_check',$userModules) && !myInArray('offers',$userModules))
+                            {
+                                ?>
+                                <li><a href="<?php echo base_url();?>offers/check"><i class="fa fa-trophy"></i> Offers Check</a></li>
+                                <?php
+                            }
+
+                            if(myInArray('checkins',$userModules))
+                            {
+                                ?>
+                                <li><a href="<?php echo base_url();?>check-ins"><i class="fa fa-calendar-check-o"></i> Check-Ins</a></li>
                                 <?php
                             }
                         }
-                        else
-                        {
-                            ?>
-                            <li><a href="<?php echo base_url();?>quiz"><i class="fa fa-question-circle-o"></i> Quiz</a></li>
-                            <?php
-                        }
-                    }
-                    if(myInArray('quiz_questions',$userModules))
-                    {
                         ?>
-                        <li><a href="<?php echo base_url();?>quiz/manageQuestions"><i class="fa fa-list-ol"></i> Quiz Questions</a></li>
-                        <?php
-                    }
-                    if(myInArray('career',$userModules))
-                    {
-                        ?>
-                        <li><a href="<?php echo base_url();?>career"><i class="fa fa-suitcase"></i> Careers</a></li>
-                        <?php
-                    }
-                    if(myInArray('mug_club',$userModules))
-                    {
-                        ?>
-                        <li><a href="<?php echo base_url();?>mugclub"><i class="fa fa-beer"></i> Mug Club</a></li>
-                        <?php
-                    }
+                    </ul>
 
-                    if(myInArray('wallet_check',$userModules))
-                    {
-                        ?>
-                        <li><a href="<?php echo base_url().'wallet';?>"><i class="fa fa-money"></i> Wallet Check</a></li>
-                        <?php
-                    }
-
-                    if(myInArray('wallet_users',$userModules))
-                    {
-                        ?>
-                        <li><a href="<?php echo base_url().'empDetails';?>"><i class="fa fa-users"></i> Wallet Users</a></li>
-                        <?php
-                    }
-
-                    if(myInArray('user_mgt',$userModules))
-                    {
-                        ?>
-                        <li><a href="<?php echo base_url();?>users"><i class="fa fa-user"></i> Users List</a></li>
-                        <?php
-                    }
-
-                    if(myInArray('location_mgt',$userModules))
-                    {
-                        ?>
-                        <li><a href="<?php echo base_url();?>locations"><i class="fa fa-globe"></i> Locations</a></li>
-                        <?php
-                    }
-
-                    if(myInArray('mailers',$userModules))
-                    {
-                        ?>
-                        <li><a href="<?php echo base_url();?>mailers"><i class="fa fa-envelope"></i> Mailers</a>
-                            <div class="notification-indicator"></div>
-                        </li>
-                        <?php
-                    }
-
-                    if(myInArray('offers',$userModules))
-                    {
-                        ?>
-                        <li><a href="<?php echo base_url();?>offers"><i class="fa fa-trophy"></i> Offers</a></li>
-                        <?php
-                    }
-
-                    if(myInArray('offers_check',$userModules) && !myInArray('offers',$userModules))
-                    {
-                        ?>
-                        <li><a href="<?php echo base_url();?>offers/check"><i class="fa fa-trophy"></i> Offers Check</a></li>
-                        <?php
-                    }
-
-                    if(myInArray('checkins',$userModules))
-                    {
-                        ?>
-                        <li><a href="<?php echo base_url();?>check-ins"><i class="fa fa-calendar-check-o"></i> Check-Ins</a></li>
-                        <?php
-                    }
-                }
-                ?>
+                </li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 <?php
